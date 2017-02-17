3096b0739d634a2f32569dea8947bf96d2db5955
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2017-02-16 21:30:09 -0500

    commit

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
 </head>
 
 <body>
-Hello World 
+Hello World <br />
 <?php echo "Welcome to sample-with-php"; ?>
+<br  />
+A cat picture:
+<?php 
+echo "<img src='https://akphoto4.ask.fm/2e5/5fd32/24fe/40d4/b765/5d0887902a95/original/210122.jpg' />";
+?>
 </body>
 </html>
